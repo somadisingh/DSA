dcf10de06928bb20891129527858de6647131c00
somadisingh <btech10001.19@bitmesra.ac.in>
2022-07-14 18:24:35 +0530

    added comments in smallest range k sorted lists

--- smallest_range_ksortedlists.cpp
+++ smallest_range_ksortedlists.cpp
@@ -30,6 +30,8 @@ int kSorted(vector<vector<int>> &a, int k, int n)
 {
     int mini = INT_MAX, maxi = INT_MIN;
     priority_queue<node *, vector<node *>, compare> minHeap;
+
+    // step 1: create a min heap for starting element of each list and tracking the min and max val
     for (int i = 0; i < k; i++)
     {
         int element = a[i][0];
@@ -40,25 +42,36 @@ int kSorted(vector<vector<int>> &a, int k, int n)
 
     int start = mini, end = maxi;
 
+    // process the ranges
     while (!minHeap.empty())
     {
+        // fetch the mini from top of heap
         node *temp = minHeap.top();
         mini = temp->data;
+
         minHeap.pop();
 
+        // range updation
         if (maxi - mini < end - start)
         {
             start = mini;
             end = maxi;
         }
 
+        // does the next element exist?
+
+        // YES
         if (temp->col + 1 < n)
         {
             maxi = max(maxi, a[temp->row][temp->col + 1]);
             minHeap.push(new node(a[temp->row][temp->col + 1], temp->row, temp->col + 1));
         }
+
+        // NO
         else
             break;
     }
+
+    // answer: difference of range
     return (end - start + 1);
 }
