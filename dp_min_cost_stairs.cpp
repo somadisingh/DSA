f93f21d86caf374888343f033106b2df02dbf82d
Somaditya <btech10001.19@bitmesra.ac.in>
2022-08-04 18:17:54 +0530

    min cost stairs tabulation method

--- dp_min_cost_stairs.cpp
+++ dp_min_cost_stairs.cpp
@@ -28,3 +28,15 @@ public:
         return ans;
     }
 };
+
+// int n = cost.size();
+//         vector<int>dp(n+1,-1);
+//         dp[0]=cost[0];
+//         dp[1]=cost[1];
+//         for (int i = 2; i < n; i++) {
+//             dp[i]=cost[i]+min(dp[i-1], dp[i-2]);
+//         }
+
+//         int ans;
+//         ans = min(dp[n-1], dp[n-2]);
+//         return ans;
