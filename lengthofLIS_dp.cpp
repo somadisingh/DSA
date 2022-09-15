56098dfe6172935b5086e3dec733e3aa83e01282
Somaditya <btech10001.19@bitmesra.ac.in>
2022-09-15 20:29:24 +0530

    LC 2007. Find Original Array From Doubled Array

--- lengthofLIS_dp.cpp
+++ lengthofLIS_dp.cpp
@@ -32,6 +32,30 @@ public:
     }
 };
 
+// tabulation
+
+int lengthOfLIS(vector<int> &arr)
+{
+
+    int n = arr.size();
+    vector<vector<int>> dp(n + 1, vector<int>(n + 1, 0));
+    for (int ind = n - 1; ind >= 0; ind--)
+    {
+        for (int prev = ind - 1; prev >= -1; prev--)
+        {
+
+            int notPick = dp[ind + 1][prev + 1];
+            int pick = 0;
+            if (prev == -1 || arr[ind] > arr[prev])
+            {
+                pick = 1 + dp[ind + 1][ind + 1];
+            }
+            dp[ind][prev + 1] = max(pick, notPick);
+        }
+    }
+    return dp[0][0];
+}
+
 int main()
 {
     Solution s;
