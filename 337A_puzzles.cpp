cacbbd65add4caca373cf7b081591b835389d58d
somadisingh <btech10001.19@bitmesra.ac.in>
2023-12-08 12:16:49 +0530

    337A optimized solution

--- 337A_puzzles.cpp
+++ 337A_puzzles.cpp
@@ -35,3 +35,29 @@ int main()
     cout<<diffinal;
     return 0;
 }
+
+/*
+MORE OPTIMIZED SOLUTION: TC O(nlogn)
+#include<bits/stdtr1c++.h>
+using namespace std;
+ 
+int main (){
+    int m,n;
+    cin >> n>>m;
+    int a[m];
+    for (int i=0; i<m; i++) cin >> a[i];
+ 
+    int maxi=INT_MIN;
+    int mini=INT_MAX;
+    sort(a,a+m);
+    int ans=INT_MAX;
+    for(int i=m-n;i>=0;i--)
+    {
+        int temp=a[i+n-1]-a[i];
+        ans=min(temp,ans);
+    }
+    cout<<ans;
+
+ 
+}
+*/
